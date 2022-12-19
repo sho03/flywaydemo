4fbb104bdf40865a80e3f8e000be311eeb1cd76a
sho ochiai <ochiai_s@welmo.co.jp>
2022-12-19 18:27:05 +0900

    feat: create project from Spring initializr

---
+++ src/main/kotlin/com/example/flywaydemo/FlywaydemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.flywaydemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FlywaydemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<FlywaydemoApplication>(*args)
+}
